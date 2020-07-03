ce8ecd614d7f40f516adb8ed31206b1d53a68899
5trange <adithmurali@protonmail.com>
2020-07-03 12:38:41 +0530

    Update main.py
    Now the program shows IPs before and after running.

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from pythonping import ping
+import requests
 import subprocess
 import time
 response_list=ping('155.133.232.98')
 print("==FireLat v1.0== \nSimple utility to fix ISP routing issues.")
 lat=response_list.rtt_avg_ms
-print("Current Ping= ",lat,"ms")
-while lat>45:
+print("IP before run-time: ",requests.get("http://ipconfig.in/ip").text)
+print("Latency before run-time= ",lat,"ms")
+while lat>49:
     print("Restarting Modem..")
     subprocess.run('restart.exe')
     time.sleep(63)
     print("Restart Complete.")
     response_list=ping('155.133.232.98')
+    ip=getip.get()
     lat=response_list.rtt_avg_ms
-    if lat<45:
+    if lat<49:
         break
 print("Current latency= ",lat,"ms")
+print("IP address: ",requests.get("http://ipconfig.in/ip").text)
 input("Press Enter to Quit.")
     
