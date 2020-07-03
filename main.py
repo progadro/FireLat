970293c953d08d385cf814c6456b1161c2cf707c
5trange <adithmurali@protonmail.com>
2020-07-03 11:36:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from pythonping import ping
+import subprocess
+import time
 response_list=ping('155.133.232.98')
-print("==FireLat v1.0== \nSimple utlity to fix ISP routing issues \nFixing routes..")
+print("==FireLat v1.0== \nSimple utlity to fix ISP routing issues.")
 print("Current Ping= ",response_list.rtt_avg_ms)
-
-
+while response_list.rtt_avg_ms>39:
+    print("Restarting Modem..")
+    subprocess.run('restart.exe')
+    time.sleep(63)
+    print("Restart Complete.")
+    response_list=ping('155.133.232.98')
+    if response_list.rtt_avg_ms>39:
+        print("Current latency= ",response_list.rtt_avg_ms)
+        break
+    else:
+        continue
+print("Current latency= ",response_list.rtt_avg_ms)
+    
+    
