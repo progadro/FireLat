47bc38dba92fdbbbc162f2de621f1923bb38ee69
5trange <adithmurali@protonmail.com>
2020-07-03 11:57:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from pythonping import ping
 import subprocess
 import time
 response_list=ping('155.133.232.98')
-print("==FireLat v1.0== \nSimple utlity to fix ISP routing issues.")
-print("Current Ping= ",response_list.rtt_avg_ms)
+print("==FireLat v1.0== \nSimple utility to fix ISP routing issues.")
+print("Current Ping= ",response_list.rtt_avg_ms,"ms")
 while response_list.rtt_avg_ms>45:
     print("Restarting Modem..")
     subprocess.run('restart.exe')
@@ -11,10 +11,10 @@ while response_list.rtt_avg_ms>45:
     print("Restart Complete.")
     response_list=ping('155.133.232.98')
     if response_list.rtt_avg_ms>45:
-        print("Current latency= ",response_list.rtt_avg_ms)
+        print("Current latency= ",response_list.rtt_avg_ms,"ms")
         break
     else:
         continue
-print("Current latency= ",response_list.rtt_avg_ms)
+print("Current latency= ",response_list.rtt_avg_ms,"ms")
 input("Press Enter to Quit.")
     
