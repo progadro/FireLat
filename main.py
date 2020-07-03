4038ec0ecbd9601b93d432ffd36b47383d390d35
5trange <adithmurali@protonmail.com>
2020-07-03 13:02:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ import time
 response_list=ping('155.133.232.98')
 print("==FireLat v1.0== \nSimple utility to fix ISP routing issues.")
 lat=response_list.rtt_avg_ms
-print("IP before run-time: ",requests.get("http://ipconfig.in/ip").text)
 print("Latency before run-time= ",lat,"ms")
+print("IP before run-time: ",requests.get("http://ipconfig.in/ip").text)
 while lat>49:
     print("Restarting Modem..")
     subprocess.run('restart.exe')
     time.sleep(63)
     print("Restart Complete.")
     response_list=ping('155.133.232.98')
-    ip=getip.get()
+    print("Latency after restart= ",lat,"ms")
     lat=response_list.rtt_avg_ms
     if lat<49:
         break
-print("Current latency= ",lat,"ms")
+print("Final latency= ",lat,"ms")
 print("IP address: ",requests.get("http://ipconfig.in/ip").text)
 input("Press Enter to Quit.")
     
