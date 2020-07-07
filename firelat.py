403cd4cfa30e480a33f07f5d06dd8c8ebd51cd33
5trange <adithmurali@protonmail.com>
2020-07-07 14:00:48 +0530

    Added firelat.py

--- firelat.py
+++ firelat.py
@@ -15,6 +15,20 @@ username.send_keys('admin')
 password.send_keys("messb0i5trange")
 capcha_verifier.send_keys(capcha_code)
 capcha_verifier.send_keys(Keys.RETURN)
+browser.switch_to.frame('topFrame')
+browser.execute_script("on_catolog('1')")
+browser.execute_script("on_menu('1')")
+browser.switch_to.parent_frame()
+browser.switch_to.frame('mainFrame')
+browser.execute_script("on_submit('sv')")
+
+
+
+
+
+
+
+
 
 
 
